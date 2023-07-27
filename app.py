3fd3d043029e857138a0a29e806d4fd106bc345c
HLTI-Automations <134806097+HLTI-Automations@users.noreply.github.com>
2023-07-26 22:07:35 -0300

    include uvicorn

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import uvicorn
 
 app = Flask(__name__)
 
@@ -7,4 +8,4 @@ def hello_world():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    uvicorn.run(host="0.0.0.0", port=5000)
