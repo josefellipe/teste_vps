05b65d0bf8b75d480c48310de1315cd90740b917
HLTI-Automations <134806097+HLTI-Automations@users.noreply.github.com>
2023-07-29 10:40:07 -0300

    change to fastapi

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from fastapi import FastAPI
 import uvicorn
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route("/")
+@app.get("/")
 def hello_world():
-    return "Hello, World!!!"
+    return {"message": "Hello, World!!!"}
 
 if __name__ == "__main__":
-    uvicorn.run(host="0.0.0.0", port=5000)
+    uvicorn.run(
+        host="0.0.0.0", 
+        port=8000
+        )
