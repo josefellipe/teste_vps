caba4cee073a759235234cb2378c260eba0349a5
HLTI-Automations <134806097+HLTI-Automations@users.noreply.github.com>
2023-08-18 20:17:56 -0300

    colocando cors

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import uvicorn
 
 app = FastAPI()
 
+# Configurações do CORS
+origins = ["*"]  # Ou você pode definir uma lista de origens permitidas aqui
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/", response_class=JSONResponse)
 async def custom_response():
     content = {"message": "Tá dando certo"}
-    response = JSONResponse(
-        content=content, 
-        status_code=200, 
-        headers={"Access-Control-Allow-Origin": "*"}
-    )
+    response = JSONResponse(content=content, status_code=200)
     return response
-
 
 if __name__ == "__main__":
     uvicorn.run(
-        host="89.117.33.83", 
-        port=8000
-        )
+        app, host="0.0.0.0", port=8000
+    )
--- lockfile.py
+++ lockfile.py
@@ -1,4 +1,4 @@
 # gunicorn app:app -c gunicorn_config.py
 
-bind = "0.0.0.0:5000"
+bind = "0.0.0.0:8000"
 workers=4
